bd916942f2eea84c4eaea75eb7650ddfd729cbcb
Rasiha Krishnan <rasiha2006@gmail.com>
2025-06-24 19:34:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circle");
+
+let currentStep = 1;
+
+next.addEventListener("click", () => {
+  currentStep++;
+  if (currentStep > circles.length) {
+    currentStep = circles.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentStep--;
+  if (currentStep < 1) {
+    currentStep = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentStep) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const activeCircles = document.querySelectorAll(".circle.active").length;
+  const progressWidth = ((activeCircles - 1) / (circles.length - 1)) * 100;
+  progress.style.width = progressWidth + "%";
+
+  prev.disabled = currentStep === 1;
+  next.disabled = currentStep === circles.length;
+}
+
 
